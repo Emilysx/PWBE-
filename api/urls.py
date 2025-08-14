a7f699b4c7db4ddefe7ff896c7fd0735a0157ce6
unknown <ds@ESN501D1278485.educ501.local>
2025-08-14 16:56:04 -0300

    Aula 04 commit 2 - 14/08/2025

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import *
+from .views import AutoresView, visu_autor
 
 urlpatterns = [ 
-    path('autores', AutoresView.as_view())
+    path('autores', AutoresView.as_view()),
+    path('autores/lista',visu_autor )
 ]
 # é uma lista que define como as URLs. ela diz ao Django qual função de visualização (view) deve ser executada 
 # quando um usuário acessa uma determinada URL. 
